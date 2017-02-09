89e9c869cfe325c614ee2c9aa2228f5f206bb825
Rizki <rizkiamrullah38@gmail.com>
2017-02-09 07:47:29 +0700

    Add project files.

---
+++ asp_mvc_2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Asp_mvc_2.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Asp_mvc_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+        [AuthorizeRoles("Admin")]
+        public ActionResult AdminOnly()
+        {
+            return View();
+        }
+
+        public ActionResult UnAuthorized()
+        {
+            return View();
+        }
+    }
+
+}
